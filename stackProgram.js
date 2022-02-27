38f36452f0da0134e55d3e962c5283c450becfc4
Vanshh29 <98050930+Vanshh29@users.noreply.github.com>
2022-02-27 16:59:51 +0100

    calling method
    calling push, pop and peep method

--- stackProgram.js
+++ stackProgram.js
@@ -40,3 +40,27 @@ class StackProgram{
         return this.collection[this.count-1]
          
      }
+const stack = new StackProgram();
+stack.push(2);
+stack.push(3);
+stack.push(5);
+stack.push(7);
+stack.push(9);
+stack.push(11);
+stack.push(13);
+stack.push(15);
+stack.push(17);
+stack.push(19);
+stack.push(20);
+stack.push(22);
+stack.push(24);
+stack.push(26);
+stack.push(28);
+stack.push(30);
+stack.push(32);
+stack.push(34);
+stack.pop();
+stack.peek();
+stack.push(100);
+stack.peek();
+stack.push(50);
