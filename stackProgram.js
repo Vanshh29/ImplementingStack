ac4bd9a9e91a62f30b0dcf7a433f96a0824748c5
Vanshh29 <98050930+Vanshh29@users.noreply.github.com>
2022-02-27 15:42:21 +0100

    push method modified
    if else condition implemented

--- stackProgram.js
+++ stackProgram.js
@@ -10,3 +10,12 @@ class StackProgram{
         if(this.count>this.count_limit){
             console.log("Count Limit of Stack Exceeded");
         }
+         else{
+
+        this.collection[this.count]=stack_collection;
+            console.log(`${stack_collection} added to ${this.count}`);
+            this.count=this.count+1;
+        
+            return this.count-1;
+        }
+    }
