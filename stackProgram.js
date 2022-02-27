ce471d23dbed645688cc9455c3daa548694ba8cf
Vanshh29 <98050930+Vanshh29@users.noreply.github.com>
2022-02-27 15:32:48 +0100

    Push method
    created push method

--- stackProgram.js
+++ stackProgram.js
@@ -5,3 +5,8 @@ class StackProgram{
         this.count=0;
         this.count_limit=15;
     }
+ push (stack_collection){
+
+        if(this.count>this.count_limit){
+            console.log("Count Limit of Stack Exceeded");
+        }
